4e517261ed81464c94290e90e3d4b688f0a8ae98
Christine Martino <christine@Christines-MBP.home>
2019-04-23 23:02:23 -0400

    Add Layout with GraphQL

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,40 @@
+import React, { Fragment } from "react"
+import PropTypes from "prop-types"
+import Header from "../header"
+import { StaticQuery, graphql } from "gatsby"
+import "../../css/main.scss"
+
+class Layout extends React.Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+  }
+
+  render() {
+    const { data, children } = this.props
+
+    return (
+      <Fragment>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div className="grim-layout">
+          <main>{children}</main>
+          <footer>© {new Date().getFullYear()}</footer>
+        </div>
+      </Fragment>
+    )
+  }
+}
+
+export default props => (
+  <StaticQuery
+    query={graphql`
+      query HeadingQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => <Layout data={data} {...props} />}
+  />
+)
