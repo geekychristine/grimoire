3f9bc143afe588cdd2202b71835a24fd93a189f7
Christine Martino <christine@Christines-MBP.home>
2019-04-23 23:40:45 -0400

    Add Spell page

---
+++ src/pages/spell.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+
+import Layout from "../components/Layout/Layout"
+import SEO from "../components/seo"
+
+class SpellPage extends Component {
+  static propTypes = {
+    location: PropTypes.object,
+  }
+
+  render() {
+    const { location } = this.props
+    console.warn("this.props:", this.props)
+
+    return (
+      <Layout>
+        <SEO title="Spell" />
+        <h1>Hi from the second page</h1>
+        <p>Welcome to page 2</p>
+        <Link to="/">Go back to the homepage</Link>
+      </Layout>
+    )
+  }
+}
+
+export default SpellPage
