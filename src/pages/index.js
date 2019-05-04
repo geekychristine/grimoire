dc7fe2f59a397071b10ac09625df05455689ce0e
Christine Martino <christine@Christines-MBP.home>
2019-05-04 18:14:19 -0400

    Get all spells

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,72 +1,82 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
-import axios from "axios"
-import { Link } from "gatsby"
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import { Link } from "gatsby";
 
-import Layout from "../components/Layout/Layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/Layout/Layout";
+import SEO from "../components/seo";
+
+// import SpellIndex from "../containers/SpellIndex";
 
 class IndexPage extends Component {
   static propTypes = {
     children: PropTypes.node,
-    baseClass: PropTypes.string,
-  }
+    baseClass: PropTypes.string
+  };
 
   static defaultProps = {
-    baseClass: "grim-home-page",
-  }
+    baseClass: "grim-home-page"
+  };
 
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.getSpellList = this.getSpellList.bind(this)
+    this.getSpellList = this.getSpellList.bind(this);
 
     this.api = {
-      spells: "http://dnd5eapi.co/api/spells",
-    }
+      spells: "/api/spells"
+    };
 
     this.state = {
-      spells: [],
-    }
+      loading: false,
+      spells: []
+    };
   }
 
-  componentDidMount() {
-    this.getSpellList()
+  async componentDidMount() {
+    axios({ url: this.api.spells })
+      .then(response => {
+        console.warn("response:", response);
+        this.setState({
+          loading: false,
+          spells: response.data.results
+        });
+      })
+      .catch(error => {
+        this.setState({ loading: false, error });
+      });
   }
 
   getSpellList() {
     // const res = await fetch(this.api.spells)
     // const data = await res.json()
     this.setState({
-      loading: true,
-    })
-    console.warn("this.api.spells:", this.api.spells)
+      loading: true
+    });
+    console.warn("getSpellList: this.api.spells:", this.api.spells);
     axios({ url: this.api.spells })
       .then(response => {
-        console.warn("response:", response)
+        console.warn("response:", response);
         this.setState({
           loading: false,
-          spells: response.data.results,
-        })
+          spells: response.data.results
+        });
       })
       .catch(error => {
-        this.setState({ loading: false, error })
-      })
+        this.setState({ loading: false, error });
+      });
   }
 
   render() {
-    const { spells, loading } = this.state
+    const { spells, loading } = this.state;
     return !loading ? (
       <Layout>
         <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
+        <h1>Spell List:</h1>
         <ul>
-          {spells &&
+          {spells && spells.length ? (
             spells.map((spell, key) => {
-              const spellId = key + 1
+              const spellId = key + 1;
               // const spellName = spell.name.replace(" ", "-").toLowerCase()
               return (
                 <li key={spellId}>
@@ -74,18 +84,17 @@ class IndexPage extends Component {
                     {spell.name}
                   </Link>
                 </li>
-              )
-            })}
+              );
+            })
+          ) : (
+            <li>No Spells Listed</li>
+          )}
         </ul>
-        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-          <Image />
-        </div>
-        <Link to="/spell/?id=1">Go to page 2</Link>
       </Layout>
     ) : (
       <p>Loading...</p>
-    )
+    );
   }
 }
 
-export default IndexPage
+export default IndexPage;
