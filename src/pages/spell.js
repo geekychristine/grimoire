8f6db6473a2b07c4e86f849a1742d312fa89dbfd
Christine Martino <christine@Christines-MBP.home>
2019-05-05 01:04:18 -0400

    Remove extra call from spell.js

--- src/pages/spell.js
+++ src/pages/spell.js
@@ -1,72 +1,10 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { fetcher } from "../utils/fetch";
 
 import Layout from "../components/Layout/Layout";
 import SEO from "../components/seo";
 import Spell from "../containers/spell";
 
 class SpellPage extends Component {
-  static propTypes = {
-    children: PropTypes.node,
-    baseClass: PropTypes.string
-  };
-
-  static defaultProps = {
-    baseClass: "gr-home-page"
-  };
-
-  constructor(props) {
-    super(props);
-
-    console.warn("props:", props);
-
-    this.getSpellList = this.getSpellList.bind(this);
-    this.successResponse = this.successResponse.bind(this);
-    this.failureResponse = this.failureResponse.bind(this);
-
-    this.api = {
-      spells: "/api/spells"
-    };
-
-    this.state = {
-      loading: false,
-      error: null,
-      spells: []
-    };
-  }
-
-  async componentDidMount() {
-    this.getSpellList();
-  }
-
-  successResponse(response) {
-    this.setState({
-      ...response,
-      spells: response.body.results
-    });
-  }
-
-  failureResponse(response) {
-    this.setState({
-      loading: false,
-      error: response.body,
-      spells: []
-    });
-  }
-
-  async getSpellList() {
-    this.setState({
-      loading: true
-    });
-
-    await fetcher(
-      { url: this.api.spells },
-      this.successResponse,
-      this.failureResponse
-    );
-  }
-
   render() {
     return (
       <Layout>
