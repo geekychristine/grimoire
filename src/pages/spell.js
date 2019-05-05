87520dc424b3ff49fb90bc06dc4f57e542b3a435
Christine Martino <christine@Christines-MBP.home>
2019-05-04 22:44:51 -0400

    Add fetcher

--- src/pages/spell.js
+++ src/pages/spell.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { fetcher } from "../utils/fetch";
 
 import Layout from "../components/Layout/Layout";
 import SEO from "../components/seo";
@@ -7,8 +8,64 @@ import Spell from "../containers/spell";
 
 class SpellPage extends Component {
   static propTypes = {
-    location: PropTypes.object
+    children: PropTypes.node,
+    baseClass: PropTypes.string
   };
+
+  static defaultProps = {
+    baseClass: "grim-home-page"
+  };
+
+  constructor(props) {
+    super(props);
+
+    console.warn("props:", props);
+
+    this.getSpellList = this.getSpellList.bind(this);
+    this.successResponse = this.successResponse.bind(this);
+    this.failureResponse = this.failureResponse.bind(this);
+
+    this.api = {
+      spells: "/api/spells"
+    };
+
+    this.state = {
+      loading: false,
+      error: null,
+      spells: []
+    };
+  }
+
+  async componentDidMount() {
+    this.getSpellList();
+  }
+
+  successResponse(response) {
+    this.setState({
+      ...response,
+      spells: response.body.results
+    });
+  }
+
+  failureResponse(response) {
+    this.setState({
+      loading: false,
+      error: response.body,
+      spells: []
+    });
+  }
+
+  async getSpellList() {
+    this.setState({
+      loading: true
+    });
+
+    await fetcher(
+      { url: this.api.spells },
+      this.successResponse,
+      this.failureResponse
+    );
+  }
 
   render() {
     return (
