9aa6973cc8798c9f932caf621b1ec6af028e4531
Christine Martino <christine.martino@thinkcompany.com>
2019-05-06 21:22:12 -0400

    Add Open Sans font

--- src/html.js
+++ src/html.js
@@ -12,7 +12,7 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <link
-          href="https://fonts.googleapis.com/css?family=Tulpen+One"
+          href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300"
           rel="stylesheet"
         />
         {props.headComponents}
